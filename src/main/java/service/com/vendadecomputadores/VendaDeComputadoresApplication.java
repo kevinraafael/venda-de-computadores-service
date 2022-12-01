0228873b357bf163f29e2cd574b5d235711c0068
kevinraafael <kevinfrancelino@gmail.com>
2022-12-01 09:10:28 -0400

    feat:initial commit with spring configuration

---
+++ src/main/java/service/com/vendadecomputadores/VendaDeComputadoresApplication.java
@@ -0,0 +1,13 @@
+package service.com.vendadecomputadores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaDeComputadoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaDeComputadoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/service/com/vendadecomputadores/VendaDeComputadoresApplicationTests.java
@@ -0,0 +1,13 @@
+package service.com.vendadecomputadores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaDeComputadoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
